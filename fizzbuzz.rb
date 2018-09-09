a21d308f10a9c941583e380ad5217cc595116b45
Randy Herasme <randyherasme6@gmail.com>
2018-09-09 05:28:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  
+def fizzbuzz (x)
+  if (x%3==0 && x%5==0)
+    return "fizzbuzz"
+  elsif (x%3==0)
+    return "fizz"
+  elsif (x%5==0)
+    return "buzz"
+  else  
+    return nil 
+  end
 end
